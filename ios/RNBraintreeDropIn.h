32ce966d7d8480a192859a316007916099305386
Brady Shober <brady.shober@gmail.com>
2019-06-26 19:54:44 -0400

    Check if Apple Pay Cancelled

--- ios/RNBraintreeDropIn.h
+++ ios/RNBraintreeDropIn.h
@@ -31,6 +31,8 @@
 
 @property (nonatomic) RCTPromiseResolveBlock _Nonnull resolve;
 
+@property (nonatomic, strong) BOOL *_Nonnull applePayAuthorized;
+
 + (void)resolvePayment:(BTDropInResult* _Nullable)result deviceData:(NSString * _Nonnull)deviceDataCollector resolver:(RCTPromiseResolveBlock _Nonnull)resolve;
 
 @end
