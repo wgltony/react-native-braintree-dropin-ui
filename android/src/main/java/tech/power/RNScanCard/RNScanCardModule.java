a16432349a17e0c2a033918aab027b628585ff74
thanushan <thanushan@calcey.com>
2020-06-24 14:09:32 +0530

    Sample toast added for scan card

--- android/src/main/java/tech/power/RNScanCard/RNScanCardModule.java
+++ android/src/main/java/tech/power/RNScanCard/RNScanCardModule.java
@@ -1,27 +1,20 @@
 package tech.power.RNScanCard;
 
-import android.app.Activity;
-import android.content.Intent;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.ReadableMap;
-import com.facebook.react.bridge.Arguments;
-import com.facebook.react.bridge.WritableMap;
-import com.facebook.react.bridge.ActivityEventListener;
-import com.facebook.react.bridge.BaseActivityEventListener;
-import com.facebook.react.bridge.Promise;
+
+import android.widget.Toast;
 
 public class RNScanCardModule extends ReactContextBaseJavaModule {
+  private static ReactApplicationContext reactContext;
 
-  private Promise mPromise;
-  private static final int DROP_IN_REQUEST = 0x444;
+  private static final String DURATION_SHORT_KEY = "SHORT";
+  private static final String DURATION_LONG_KEY = "LONG";
 
-  private boolean isVerifyingThreeDSecure = false;
-
-  public RNScanCardModule(ReactApplicationContext reactContext) {
-    super(reactContext);
-    reactContext.addActivityEventListener(mActivityListener);
+  RNScanCardModule(ReactApplicationContext context) {
+    super(context);
+    reactContext = context;
   }
 
   @ReactMethod
