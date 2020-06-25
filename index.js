f5be84b3d21e0e1e2259113ebfdbc8a89b1b0d20
Risina Perera <risina@calcey.com>
2020-06-25 16:42:25 +0530

    Fix minor error.

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ import { NativeModules } from 'react-native';
 
 const { RNBraintreeDropIn, RNScanCard } = NativeModules;
 
-export default {RNBraintreeDropIn.  RNScanCard};
+export default {RNBraintreeDropIn,  RNScanCard};
